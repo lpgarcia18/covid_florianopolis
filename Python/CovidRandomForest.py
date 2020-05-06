dbc970d75d715691629f6468161cf2196ea232f4
avgandre <61151414+avgandre@users.noreply.github.com>
2020-05-05 23:14:47 -0300

    Add files via upload
    Nova versão da implementação em Random Forest

--- Python/CovidRandomForest.py
+++ Python/CovidRandomForest.py
@@ -8,34 +8,47 @@ Created on Mon Apr 20 16:48:01 2020
 
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
 
-df = pd.read_csv("Dados/covid_ajustado.csv")
+#Faz a leitura da base
+df = pd.read_csv("Dados/novo_covid_ajustado.csv")
+
+#Definindo x, y
+features = df.columns.difference(['RESULTADO'])
+x = df[features].values
+y = df['RESULTADO'].values
 
 #Balanceamento
-qtdeDescartados, qtdeConfirmados = df['Resultado_do_teste'].value_counts().values
-dfDescartados = df[df['Resultado_do_teste'] == 'descartado'] #Separa a base de descartados
-dfConfirmados = df[df['Resultado_do_teste'] == 'confirmado'] #Separa a base de confirmados
+qtdeDescartados, qtdeConfirmados = df['RESULTADO'].value_counts().values
+dfDescartados = df[df['RESULTADO'] == 'descartado'] #Separa a base de descartados
+dfConfirmados = df[df['RESULTADO'] == 'confirmado'] #Separa a base de confirmados
 
 #Under sampling
 print('\nUnder Sampling')
 dfDescartadosUnder = dfDescartados.sample(qtdeConfirmados)
 dfUnder = pd.concat([dfDescartadosUnder, dfConfirmados], axis=0)
+x = dfUnder[features].values
+y = dfUnder['RESULTADO'].values
 
 #Over sampling
-#print('\nOver Sampling')
-#dfConfirmadosOver = dfConfirmados.sample(qtdeDescartados, replace=True)
-#dfOver = pd.concat([dfDescartados, dfConfirmadosOver], axis=0)
+print('\nOver Sampling')
+dfConfirmadosOver = dfConfirmados.sample(qtdeDescartados, replace=True)
+dfOver = pd.concat([dfDescartados, dfConfirmadosOver], axis=0)
+x = dfOver[features].values
+y = dfOver['RESULTADO'].values
 
-#Definindo x, y
-features = dfUnder.columns.difference(['Resultado_do_teste'])
-x = dfUnder[features].values
-y = dfUnder['Resultado_do_teste'].values
+#Smote sampling
+print('\nOver Sampling')
+oversample = SMOTE()
+x, y = oversample.fit_resample(x, y)
 
 #Separa base treinamento e teste
-xTreino, xTeste, yTreino, yTeste = train_test_split(x, y)
+xTreino, xTeste, yTreino, yTeste = train_test_split(x, y, random_state=1986)
 
 #Classificador Randon Forest
 classifierRF = RandomForestClassifier(random_state=1986, criterion='gini', max_depth=10, n_estimators=50, n_jobs=-1)
@@ -74,5 +87,22 @@ print('Especificidade: %f%%' %
 
 #Validação cruzada
 print('\nValidação Cruzada')
-scoresDf = cross_val_score(classifierRF, xTreino, yTreino, scoring='accuracy', cv=5)
-print('Acurácia: %f%%' % (scoresDf.mean()))
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1986)
+
+#Base Treino
+scores = cross_val_score(classifierRF, xTreino, yTreino, scoring='accuracy', cv=cv)
+print('Acurácia: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+
+scoresDf = pd.DataFrame(scores)
+print(scoresDf.describe())
+scoresDf.boxplot()
+plt.show()
+
+#Base de Teste
+scores = cross_val_score(classifierRF, xTeste, yTeste, scoring='accuracy', cv=cv)
+print('Acurácia: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+
+scoresDf = pd.DataFrame(scores)
+print(scoresDf.describe())
+scoresDf.boxplot()
+plt.show()
